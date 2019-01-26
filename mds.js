14a09c32e4364df9c69a435bdbee2026eae5d1ac
rsoriano <rsorianosanchis@gmail.com>
2019-01-26 18:56:39 +0100

    git init, arbet om text-fireld

---
+++ mds.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let mds = {
+ texField: function(){
+  console.log("hola");
+  prueba('pedo')
+ },
+ switch: function () {
+  /* body... */
+ },
+ checkbox: function () {
+  /* body... */
+ },
+ radio: function () {
+  /* body... */
+ },
+};
+
+
+function prueba (arg) {
+
+ console.log("soy un cabeza de chorlito"+arg);
+ // body... 
+};
+
